9b43780eec445674785b62ab42d4ad679c71b843
Fuongfg <99278630+Puonsmt@users.noreply.github.com>
2025-04-18 21:45:03 +0700

    Update ddl_gold_layer.sql

--- schema/gold/ddl_gold_layer.sql
+++ schema/gold/ddl_gold_layer.sql
@@ -1,38 +1,22 @@
-CREATE SCHEMA gold;
-GO
-
-	-- Cơ sở dữ liệu văn phòng đại diện 
-IF OBJECT_ID ('gold.dim_state' , 'U') IS NOT NULL
-	DROP TABLE gold.dim_state;
-CREATE TABLE gold.dim_state (
-	state_key INT IDENTITY(1, 1) PRIMARY KEY,
-	state_name NVARCHAR(50)
-);
-GO
-
 IF OBJECT_ID ('gold.dim_city' , 'U') IS NOT NULL
 	DROP TABLE gold.dim_city;
 CREATE TABLE gold.dim_city (
-	city_key INT IDENTITY(1, 1) PRIMARY KEY,
-	city_code VARCHAR(20),
+	city_key VARCHAR(20) PRIMARY KEY,
 	city_name NVARCHAR(50),
-	state_key INT,
-	office_address NVARCHAR(50),
-	FOREIGN KEY (state_key) REFERENCES gold.dim_state (state_key)
+	state_name NVARCHAR(50),
+	office_address NVARCHAR(200)
 );
 GO
 
 IF OBJECT_ID ('gold.dim_customer' , 'U') IS NOT NULL
 	DROP TABLE gold.dim_customer;
 CREATE TABLE gold.dim_customer (
-	customer_key INT IDENTITY(1, 1) PRIMARY KEY,
-	customer_id VARCHAR(20),
+	customer_key VARCHAR(20) PRIMARY KEY,
 	customer_name NVARCHAR(50),
-	city_key INT,
-	first_order_date DATE,
+	city_key VARCHAR(20),
 	customer_type NVARCHAR(50),
 	tour_guide NVARCHAR(50),
-	postal_address NVARCHAR(50),
+	postal_address NVARCHAR(200),
 	FOREIGN KEY (city_key) REFERENCES gold.dim_city (city_key)
 );
 GO
@@ -40,21 +24,18 @@ GO
 IF OBJECT_ID ('gold.dim_item' , 'U') IS NOT NULL
 	DROP TABLE gold.dim_item;
 CREATE TABLE gold.dim_item (
-	item_key INT IDENTITY(1, 1) PRIMARY KEY,
-	item_id VARCHAR(20),
-	item_desc VARCHAR(50),
+	item_key VARCHAR(20) PRIMARY KEY,
+	item_desc NVARCHAR(200),
 	item_size NVARCHAR(20),
-	item_weight DECIMAL(10,2),
-	item_price DECIMAL(20,2)
+	item_weight DECIMAL(10,2)
 );
 GO
 
 IF OBJECT_ID ('gold.dim_store' , 'U') IS NOT NULL
 	DROP TABLE gold.dim_store;
 CREATE TABLE gold.dim_store (
-	store_key INT IDENTITY(1, 1) PRIMARY KEY,
-	store_id VARCHAR(20),
-	city_key INT,
+	store_key VARCHAR(20) PRIMARY KEY,
+	city_key VARCHAR(20),
 	phone_number VARCHAR(20),
 	FOREIGN KEY (city_key) REFERENCES gold.dim_city (city_key)
 );
@@ -64,7 +45,6 @@ IF OBJECT_ID ('gold.dim_time' , 'U') IS NOT NULL
 	DROP TABLE gold.dim_time;
 CREATE TABLE gold.dim_time (
 	time_key INT IDENTITY(1, 1) PRIMARY KEY,
-	time_date DATE,
 	time_year INT,
 	time_quarter INT,
 	time_month INT,
@@ -72,15 +52,14 @@ CREATE TABLE gold.dim_time (
 );
 GO
 
+--Cac bang fact 
 IF OBJECT_ID ('gold.fact_order' , 'U') IS NOT NULL
 	DROP TABLE gold.fact_order;
 CREATE TABLE gold.fact_order (
-	order_key INT IDENTITY(1, 1) PRIMARY KEY,
-	order_id VARCHAR(20),
-	customer_key INT,
+	order_key VARCHAR(20) PRIMARY KEY,
+	customer_key VARCHAR(20),
 	time_key INT,
 	total_amount DECIMAL(20,2),
-	order_count INT,
 	FOREIGN KEY (customer_key) REFERENCES gold.dim_customer (customer_key),
 	FOREIGN KEY (time_key) REFERENCES gold.dim_time (time_key)
 );
@@ -90,9 +69,9 @@ IF OBJECT_ID ('gold.fact_order_details' , 'U') IS NOT NULL
 	DROP TABLE gold.fact_order_details;
 CREATE TABLE gold.fact_order_details (
 	order_detail_key INT IDENTITY(1, 1) PRIMARY KEY,
-	order_key INT,
-	item_key INT,
-	store_key INT,
+	order_key VARCHAR(20),
+	item_key VARCHAR(20),
+	store_key VARCHAR(20),
 	quantity INT,
 	unit_price DECIMAL(20,2),
 	line_total AS (quantity * unit_price) PERSISTED,
@@ -107,13 +86,11 @@ IF OBJECT_ID ('gold.fact_inventory' , 'U') IS NOT NULL
 CREATE TABLE gold.fact_inventory (
 	inventory_key INT IDENTITY(1, 1) PRIMARY KEY,
 	time_key INT,
-	item_key INT,
-	store_key INT,
+	item_key VARCHAR(20),
+	store_key VARCHAR(20),
 	quantity_on_hand INT,
 	FOREIGN KEY (time_key) REFERENCES gold.dim_time (time_key),
 	FOREIGN KEY (item_key) REFERENCES gold.dim_item (item_key),
 	FOREIGN KEY (store_key) REFERENCES gold.dim_store (store_key)
 );
 GO
-
-
